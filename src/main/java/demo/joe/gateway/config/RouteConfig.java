87d0fa90c9e1daccd1319c70918261584daf81b4
TW - IT - BE - Joe Wu <joe.wu@shoalter.com>
2023-08-10 13:55:53 +0800

    Add sample route

--- src/main/java/demo/joe/gateway/config/RouteConfig.java
+++ src/main/java/demo/joe/gateway/config/RouteConfig.java
@@ -1,5 +1,6 @@
 package demo.joe.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +9,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouteConfig {
 
+  @Value("${login.service.base-url:https://localhost:8081}")
+  private static String loginServiceBaseUrl;
+
   @Bean
   public RouteLocator routes(RouteLocatorBuilder builder) {
     return builder.routes()
+        // example_route is the id of the route
         .route("example_route", r -> r
+//            GET is the http method of the route
+            .method("GET").and()
+//            /example is the path of the route
             .path("/example")
-            .uri("http://example.com"))
+//            /example -> / is the rewrite path of the route
+            .filters(f -> f.rewritePath("/example", "/"))
+//            LOGIN_SERVICE_URI is the uri of the route
+            .uri(loginServiceBaseUrl))
         .build();
   }
 }
